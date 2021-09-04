a8fa5fa88aa2c68d7fb49562591b4a59cd9ce6f6
AslamLert <gloommyday@gmail.com>
2021-09-04 15:55:43 +0700

    customers works!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CustomersComponent } from './components/customers/customers.component';
+
+const appRoutes: Routes = [
+  {path: '', component: CustomersComponent}
+];
+
 
 @NgModule({
   declarations: [
@@ -16,7 +22,8 @@ import { CustomersComponent } from './components/customers/customers.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
